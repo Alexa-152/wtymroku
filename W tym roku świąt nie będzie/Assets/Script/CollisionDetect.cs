0d2de7b6efae956a112d5352231adabd3f7d5996
KWgames3 <kuba.wolowiec03@wp.pl>
2019-11-15 21:04:50 +0100

    1

---
+++ W tym roku świąt nie będzie/Assets/Script/CollisionDetect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetect : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collided with " + collision.gameObject.name);
+        if(collision.gameObject.tag == "Floor")
+        {
+
+            GetComponentInParent<poruszanie>().isGrounded = true;
+        }
+    }
+}
